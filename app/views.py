10a10fc2d18a341f65f3f84c95633aefd0559dd0
Matthew Hughes <me@matthewhughes.co.uk>
2013-08-13 21:48:04 +0100

    Updated stuff

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from app import app
 
+homepage = ''' 
+<h2>Really Scrapable Web App is a tool to learn about data science</h1>
+<p>Super cool, right? There are lots of things to scrape on this website. But let's start off easy.</p>
+<h1>Write a scraper that yanks this line and prints it to the console </h1>
+'''
+
 @app.route('/')
 @app.route('/index')
 
@@ -14,3 +20,9 @@ def contactus():
 @app.route('/challengeone')
 def challengeone():
     return render_template('challengeone.html')
+
+@app.route('/challengetwo')
+def challengetwo():
+    return render_template('challengetwo.html')
+
+
