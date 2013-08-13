e1822ae1c9372cf425d084785bd6aed4961ce260
Matthew Hughes <me@matthewhughes.co.uk>
2013-08-13 15:21:21 +0100

    adding pre auth pages

--- app/views.py
+++ app/views.py
@@ -3,5 +3,14 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+
 def index():
     return render_template('index.html')
+
+@app.route('/contactus')
+def contactus():
+    return render_template('contactus.html')
+
+@app.route('/challengeone')
+def challengeone():
+    return render_template('challengeone.html')
