27a266f2fa6e71536fe68fb6cb12c58fcc432fb1
Matthew Hughes <me@matthewhughes.co.uk>
2013-08-18 17:13:27 +0100

    Added forms.py

---
+++ app/forms.py
@@ -0,0 +1,6 @@
+from flask.ext.wtf import Form, TextField, BooleanField
+from flask.ext.wtf import Required
+
+class LoginForm(Form):
+    username = TextField('username', validators = [Required()])
+    password = TextField('password', validators = [Required()])
